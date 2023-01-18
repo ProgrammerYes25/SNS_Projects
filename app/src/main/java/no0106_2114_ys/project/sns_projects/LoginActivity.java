5ce66c8bff3e561324af55ca0ea4458d841bf112
YeseoCho(yes25) <programmer.yes25@gmail.com>
2023-01-18 23:53:58 +0900

    로그인 기능구현

--- app/src/main/java/no0106_2114_ys/project/sns_projects/LoginActivity.java
+++ app/src/main/java/no0106_2114_ys/project/sns_projects/LoginActivity.java
@@ -1,8 +1,5 @@
 package no0106_2114_ys.project.sns_projects;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,25 +8,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignUpActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     private static final String TAG  = "signUpActivity";
     private FirebaseAuth mAuth; //FirevaseAuth 객체 선언
     EditText emailEditText, passwordEditText, passwordCheckEditText;
-    Button signUpButton;
+    Button signUpButton, loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
         emailEditText = findViewById(R.id.emailEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordCheckEditText = findViewById(R.id.passwordCheckEditText);
         signUpButton = findViewById(R.id.signUpButton);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(onClickListener);
         signUpButton.setOnClickListener(onClickListener);
         mAuth = FirebaseAuth.getInstance(); //FirevaseAuth 객체 정의
     }
@@ -40,41 +42,40 @@ public class SignUpActivity extends AppCompatActivity {
                 case R.id.signUpButton:
                     signUP();
                     break;
+                case R.id.loginButton:
+                    login();
+                    break;
             }
         }
     };
 
-
     private void signUP(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+    private void login(){
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
         String passwordCheck = passwordCheckEditText.getText().toString();
         if(email.length()>0 && password.length()>0){//비밀번호와 이메일 비어있는 지 확인
-            if(password.equals(passwordCheck)){//비밀번호와 비밀번호확인이 일치하는지 확인
-                //회원 가입
-                // FirevaseAuth에 있는 createUserWithEmailAndPassword는 파이어 베이스에서 회원 가입 할때 사용
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            //통신 하고 나서 무슨일을 할지
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // 로그인 성공시
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    showToast("회원가입에 성공하였습니다.");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    startLoginActivity();
-                                } else {
-                                    // 로그인 실패시
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    showToast("회원가입에 실패 하였습니다.\n    다시 확인해주세요.");
-                                }
+            // 로그인
+            // FirevaseAuth에 있는 signInUserWithEmailAndPassword는 파이어 베이스에서 로그인 할때 사용
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        //통신 하고 나서 무슨일을 할지
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // 로그인 성공시
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                startMainActivity();
+                            } else {
+                                // 로그인 실패시
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
                             }
-                        });
-            }
-            else{
-                showToast("비밀번호가 일치하지 않습니다.\n   다시 확인해주세요.");
-            }
+                        }
+                    });
         }
         else {
             showToast("이메일 또는 비밀번호를 입력해주세요.");
@@ -84,8 +85,9 @@ public class SignUpActivity extends AppCompatActivity {
     private void showToast(String msg) {
         Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
     }
-    private void startLoginActivity(){
-        Intent intent = new Intent(this, LoginActivity.class);
+
+    private void startMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/no0106_2114_ys/project/sns_projects/MainActivity.java
+++ app/src/main/java/no0106_2114_ys/project/sns_projects/MainActivity.java
@@ -2,13 +2,26 @@ package no0106_2114_ys.project.sns_projects;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth; //FirevaseAuth 객체 선언
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance(); //FirevaseAuth 객체 정의
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            startSignUpActivity();
+        }
+    }
+    private void startSignUpActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
